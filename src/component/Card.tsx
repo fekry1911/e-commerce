eedaa68fa3f66b4d8ec6c69ec4199cd20e85813d
Abdulrahman Fekry <abdofekryasda@gmail.com>
2025-09-06 04:25:52 +0300

    style

--- src/component/Card.tsx
+++ src/component/Card.tsx
@@ -1,6 +1,7 @@
 import Button from "react-bootstrap/Button";
 import Card from "react-bootstrap/Card";
 import { useNavigate } from "react-router-dom";
+import Style from "../styles/cardHover.module.css";
 
 function BasicExample({
   title,
@@ -26,7 +27,7 @@ function BasicExample({
   const navigate = useNavigate();
 
   return (
-    <Card>
+    <Card className={`h-100 d-flex flex-column ${Style.card}`}>
       <div className="position-relative">
         <Card.Img variant="top" src={image} />
 
@@ -34,22 +35,25 @@ function BasicExample({
           style={{
             backgroundColor: Stock > 20 ? "green" : "red",
           }}
-          className="position-absolute top-0 start-0 translate-middle badge rounded-pill  text-light p-2 m-5"
+          className="position-absolute top-0 start-0 translate-middle badge rounded-pill text-light p-2 m-5"
         >
           {Stock > 20 ? "In Stock" : "Out of Stock"}
         </div>
       </div>
+
       <Card.Body
         onClick={() => navigate(`/product/${id}`)}
         style={{ cursor: "pointer" }}
+        className="d-flex flex-column"
       >
-        <div className="d-flex justify-content-between ">
+        <div className="d-flex justify-content-between">
           <Card.Title style={{ fontSize: "1rem" }}>{title}</Card.Title>
           <span className="float-end d-flex gap-2">
-            <p>{price}</p>
-            <p className="text-danger">{discountPercentage}</p>
+            <p className="mb-0">{price}</p>
+            <p className="mb-0 text-danger">{discountPercentage}</p>
           </span>
         </div>
+
         <Card.Text
           style={{
             fontSize: "0.875rem",
@@ -62,6 +66,7 @@ function BasicExample({
         >
           {description}
         </Card.Text>
+
         <div className="mb-2 text-warning">
           <i className="bi bi-star-fill"></i>
           <i className="bi bi-star-fill"></i>
@@ -71,22 +76,24 @@ function BasicExample({
           <span className="text-dark ms-2">(4.0)</span>
         </div>
       </Card.Body>
-      <Button
-        onClick={() => AddToCart(id)}
-        style={{
-          margin: "0 1rem 1rem 1rem",
-          backgroundColor: cart.find((item) => item.id === id)
-            ? "green"
-            : "transparent",
-          border: "1px solid #ccc",
-          borderRadius: "30px",
-          color: cart.find((item) => item.id === id) ? "white" : "black",
-        }}
-      >
-        {cart.find((item) => item.id === id)
-          ? "Remove from Cart"
-          : "Add to Cart"}
-      </Button>
+      <div className="m-2">
+        <Button
+          onClick={() => AddToCart(id)}
+          style={{
+            width: "100%",
+            backgroundColor: cart.find((item) => item.id === id)
+              ? "green"
+              : "transparent",
+            border: "1px solid #ccc",
+            borderRadius: "30px",
+            color: cart.find((item) => item.id === id) ? "white" : "black",
+          }}
+        >
+          {cart.find((item) => item.id === id)
+            ? "Remove from Cart"
+            : "Add to Cart"}
+        </Button>
+      </div>
     </Card>
   );
 }
--- src/component/Home.tsx
+++ src/component/Home.tsx
@@ -1,15 +1,21 @@
 import React, { useState, useEffect } from "react";
 import BasicExample from "./Card";
 import { useCart } from "../context/CartContext";
+import style from "../styles/loading.module.css";
 
 export default function Home() {
   const [items, setItems] = useState<any[]>([]);
+  const [Loading, SetLoading] = useState<boolean>(true);
+
   const { cart, addToCart, removeFromCart } = useCart();
 
   useEffect(() => {
     fetch("https://dummyjson.com/products")
       .then((response) => response.json())
-      .then((data) => setItems(data.products))
+      .then((data) => {
+        setItems(data.products);
+        SetLoading(false);
+      })
       .catch((error) => console.error("Error:", error));
   }, []);
 
@@ -22,25 +28,33 @@ export default function Home() {
   };
 
   return (
-    <div className="row gap-4 justify-content-center mt-4">
-      {items.map((product) => (
-        <div
-          key={product.id}
-          className="col-12 col-sm-6 col-md-4 col-lg-3 col-xl-2"
-        >
-          <BasicExample
-            title={product.title}
-            description={product.description}
-            image={product.thumbnail}
-            Stock={product.stock}
-            price={product.price}
-            id={product.id}
-            discountPercentage={product.discountPercentage}
-            AddToCart={() => toggleCart(product)}
-            cart={cart}
-          />
+    <>
+      {Loading ? (
+        <div className={style.loaderWrapper}>
+          <div className={style.loader}></div>
         </div>
-      ))}
-    </div>
+      ) : (
+        <div className="row gap-4 justify-content-center mt-4">
+          {items.map((product) => (
+            <div
+              key={product.id}
+              className="col-12 col-sm-6 col-md-4 col-lg-3 col-xl-2"
+            >
+              <BasicExample
+                title={product.title}
+                description={product.description}
+                image={product.thumbnail}
+                Stock={product.stock}
+                price={product.price}
+                id={product.id}
+                discountPercentage={product.discountPercentage}
+                AddToCart={() => toggleCart(product)}
+                cart={cart}
+              />
+            </div>
+          ))}
+        </div>
+      )}
+    </>
   );
 }
